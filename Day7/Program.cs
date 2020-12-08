7186711dcae1a32fa0a562ba1dbdfb8edb3a363b
Ahlqvist, Johan <Johan.Ahlqvist@wartsila.com>
2020-12-08 16:37:54 +0200

    Day 8

---
+++ Day7/Program.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections; 
+using System.Diagnostics;
+using TreeCollections;
+
+namespace Day7
+{
+    static class calculator
+    {
+
+
+        public static BagNodeRoot rootItem;
+        
+
+
+        static string[] testrules = new string[]
+        {
+            "light red bags contain 1 bright white bag, 2 muted yellow bags.",
+            "dark orange bags contain 3 bright white bags, 4 muted yellow bags.",
+            "bright white bags contain 1 shiny gold bag.",
+            "muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.",
+            "shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.",
+            "dark olive bags contain 3 faded blue bags, 4 dotted black bags.",
+            "vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.",
+            "faded blue bags contain no other bags.",
+            "dotted black bags contain no other bags."
+        };
+
+        static public void populatetree()
+        {
+
+            rootItem = new BagNodeRoot();
+
+            foreach (var rule in testrules)
+            {
+                parse(rule);
+
+            }
+
+        }
+
+        static public void printtree()
+        {
+            rootItem.PrintTree();
+        }
+
+        static public void parse(string rule)
+        {
+            BagNode bag;
+            
+            string[] s = rule.Split(" bags contain ");
+
+            string containerBagColor = s[0];
+
+            string[] t = s[1].Split(", ");
+
+            var current = rootItem.AddColor(containerBagColor);
+                
+            foreach (string c in t)
+            {
+                var d = c.Split(' ');
+                int amt = 0;
+                if(int.TryParse(d[0], out amt))
+                {
+                    string color = d[1] + " " + d[2];
+
+                    for (int i = 0; i < amt; i++)
+                        current.Add(color);
+
+                }    
+                    
+            }
+
+            
+            
+        }
+
+
+    }
+
+
+    class BagNodeRoot
+    {
+        BagNode _root = new BagNode("root");
+        
+        
+        public BagNode AddColor(string value)
+        {
+            // Add nodes from string chars.
+            BagNode current = this._root;
+            current = current.Add(value);
+            return current;
+        }
+
+        public bool ContainsColor(string value)
+        {
+            // Get existing nodes from string chars.
+            BagNode current = this._root;
+            
+            current = current.Get(value);
+            if (current == null)
+            {
+                return false;
+            }
+            
+            // Return state.
+            return current != null;
+        }
+
+        public void PrintTree()
+        {
+            _root.PrintBagNode(_root);
+        }
+
+        public void MoveColorsAtRoot()
+        {
+            for (int i = _root.Bags.Count - 1; i >= 0; i--)
+            {
+                var bag = _root.Bags[i];
+                if (_root.MoveBagWithColorToNode(bag.Color, bag))
+                    _root.Remove(bag.Color);
+            }
+
+        }
+    }
+
+    class BagNode
+    {
+        List<BagNode> _bags; 
+        string _color;
+
+        internal List<BagNode> Bags { get => _bags; }
+        public string Color { get => _color; }
+
+        public BagNode(string color)
+        {
+            _color = color;
+        }
+
+        public void Remove(string color)
+        {
+            
+            for(int i = _bags.Count-1;i>=0;i--)
+            {
+                if (_bags[i]._color.Equals(color))
+                    _bags.RemoveAt(i);
+            }
+            
+        }
+
+        public BagNode Add(string color)
+        {
+            // Add individual node as child.
+            // ... Handle null field.
+            if (this._bags == null)
+            {
+                this._bags = new List<BagNode>();
+            }
+            
+            // Look up and return if possible.
+            
+            //foreach (var b in this._bags)
+            //    if (b._color.Equals(color))
+            //        return b;
+
+            // Store.
+            BagNode bag;
+            bag = new BagNode(color);
+            bag._color = color;
+            this._bags.Add(bag);
+            return bag;
+        }
+
+        public BagNode Get(string color)
+        {
+            // Get individual child node.
+            if (this._bags == null)
+            {
+                return null;
+            }
+            
+            foreach (var b in this._bags)
+                if (b._color.Equals(color))
+                    return b;
+            
+            return null;
+        }
+
+        
+        public void PrintBagNode(BagNode bagnode)
+        {
+            Console.WriteLine($"Color: {bagnode._color}");
+
+            if (bagnode._bags == null)
+                return;
+
+            foreach (var bag in bagnode._bags)
+            {
+                PrintBagNode(bag);
+            }
+        }
+
+        public bool MoveBagWithColorToNode(string color, BagNode bagnode)
+        {
+            bool success = false;
+            if (this._color.Equals(color))
+            {
+                if (this._bags == null)
+                {
+                    this._bags = new List<BagNode>();
+                    this._bags.Add(bagnode);
+                    success = true;
+                }
+
+                return success;
+            }
+            else
+            {
+                if(this._bags != null)
+                {
+                    
+                    foreach (var bag in this._bags)
+                    {
+                        if (bag.MoveBagWithColorToNode(color, bagnode))
+                            success=true;
+                    }
+                }
+                return success;
+            }
+            
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            calculator.populatetree();
+            calculator.rootItem.MoveColorsAtRoot();
+            calculator.printtree();
+        }
+    }
+}
---
+++ Day8/Program.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Day8
+{
+    static class gameconsole
+    {
+        static int accu = 0;
+        
+        public static string[] testcode = new string[]
+        {
+            "nop +0",
+            "acc +1",
+            "jmp +4",
+            "acc +3",
+            "jmp -3",
+            "acc -99",
+            "acc +1",
+            "jmp -4",
+            "acc +6"
+        };
+
+        public static string[] code;
+
+        private static MemoryOp[] memory;
+
+        private static MemoryOp[] memory2;
+
+        public static int Accu { get => accu; }
+
+        public static void getinputfromfile()
+        {
+            code = System.IO.File.ReadAllLines(@"C:\Users\JAH016\Source\Repos\AOC2020\Day8\input.txt");
+
+        }
+
+        private struct MemoryOp 
+        {
+            string _op;
+            int _value;
+            bool _changed;
+            int _called;
+
+            public MemoryOp(string op, int value, bool changed, int called) { _op = op; _value = value;  _changed = changed;  _called = called; }
+
+            public string Op 
+            {
+                get => _op;
+                set => _op = value;
+            }
+            public int Called { get => _called; set => _called = value; }
+            public bool Changed { get => _changed; set => _changed = value; }
+            public int Value { get => _value; }
+        }
+
+        private static bool change_nopjmp(ref MemoryOp mo)
+        {
+            if (mo.Op == "jmp")
+            {
+                mo.Op = "nop";
+                mo.Changed = true;
+                return true;
+            }
+            else if (mo.Op == "nop")
+            {
+                mo.Op = "jmp";
+                mo.Changed = true;
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public static void load_code_to_memory(string[] code)
+        {
+            int code_size = code.Length;
+            memory = new MemoryOp[code_size];
+
+            int i = 0;
+            foreach(var c in code)
+            {
+                memory[i] = decode(c);
+                i++;
+            }
+
+        }
+
+
+        public static void run()
+        {
+            int program_counter = 0;
+            int code_size = memory.Length;
+
+            do
+            {
+                program_counter += execute(ref memory[program_counter]);
+
+                if (program_counter < 0 || program_counter >= code_size)
+                    throw new ArgumentException("Illegal program counter value");
+
+                Console.WriteLine($"accu: {accu}");
+
+            }
+            while (memory[program_counter].Called < 1);
+            
+        }
+
+        public static bool run2(int run_num, out int changed_pc)
+        {
+            int program_counter = 0;
+            int previous_program_counter = -1;
+            int code_size = memory.Length;
+            changed_pc = -1;
+            accu = 0;
+            
+            do
+            {
+                if (changed_pc < 0 && !memory[program_counter].Changed)
+                    if(change_nopjmp(ref memory[program_counter]))
+                        changed_pc = program_counter;
+
+                program_counter += execute(ref memory[program_counter]);
+
+                if (program_counter == previous_program_counter)
+                    break;
+
+                if (program_counter < 0 || program_counter >= code_size)
+                    throw new ArgumentException("Illegal program counter value");
+
+                if (program_counter == code_size - 1 && changed_pc >= 0)
+                    return true;
+
+                previous_program_counter = program_counter;
+            }
+            while (memory[program_counter].Called < run_num);
+
+            if(changed_pc > 0)
+            {
+                // change back
+                change_nopjmp(ref memory[changed_pc]);
+            }
+            
+            return false;
+        }
+
+        static MemoryOp decode(string row)
+        {
+            string[] r = row.Split(' ');
+
+            string opcode = r[0];
+            int value = 0;
+            if (!int.TryParse(r[1], out value))
+                throw new ArgumentException($"Could not parse opcode value {r[1]}");
+
+            return new MemoryOp(opcode, value, false, 0);
+        }
+
+        static int execute(ref MemoryOp mo)
+        {
+            mo.Called++;
+            switch (mo.Op)
+            {
+                case "nop":
+                    return 1;        
+                
+                case "acc":
+                    accu += mo.Value;
+                    return 1;
+
+                case "jmp":
+                    return mo.Value;
+
+                default:
+                    throw new ArgumentException($"Unknown opcode {mo.Op}");
+            }
+
+        }
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch sw = new Stopwatch();
+            Console.WriteLine("Code of advent 2020 - Day 8");
+            sw.Start();
+            gameconsole.getinputfromfile();
+            gameconsole.load_code_to_memory(gameconsole.code);
+            int pc = 0;
+            int run_time = 0;
+            while (!gameconsole.run2(run_time, out pc))
+            {
+                Console.WriteLine($"{pc}");
+                run_time++;
+            };
+            Console.WriteLine($"Program counter: {pc}");
+            Console.WriteLine($"Accumulator: {gameconsole.Accu}");
+            sw.Stop();
+            Console.WriteLine("Time elapsed for day8 part2 .NET 5 (ms): {0}", sw.Elapsed.TotalMilliseconds);
+        }
+    }
+}
